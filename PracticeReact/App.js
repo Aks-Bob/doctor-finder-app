b0b50212dd42e5228806e7892fe9d58a58b164ea
Aks-Bob <125875424+Aks-Bob@users.noreply.github.com>
2023-03-10 04:12:06 +0000

    install all react native requirement

--- PracticeReact/App.js
+++ PracticeReact/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import NavigationDrawer from './NavigationDrawer';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Poppins-ExtraBoldItalic.ttf'),
+  });
   return (
-    <View style={ styles.container}>
-      <NavigationDrawer />
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
